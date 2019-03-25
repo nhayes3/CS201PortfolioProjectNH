36864fc42ab71129fe64c60fd0ec6081d4ab0cc9
nhayes3 <nhayes3@crimson.ua.edu>
2019-03-24 20:09:53 -0500

    pvp update

--- cfourmain.c
+++ cfourmain.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node {
-  int xCoor;
-  int yCoor;
-  int player1;
-  int player2;
-  int filled;
-  struct piece *topLeft;
-  struct piece *left;
-  struct piece *botLeft;
-  struct piece *bot;
-  struct piece *botRight;
-  struct piece *right;
-  struct piece *topRight;
-  struct piece *top;
-};
+#include <string.h>
+#include <stdbool.h>
 
 void createMenuScreen() {
   printf(" ____________________________ \n");
@@ -40,16 +26,68 @@ void createMenuScreen() {
   printf("|____________________________|\n");
 }
 
-void createBoardScreen() { //int column, int row) {
+void createBoardScreen(int rows, int cols, char board[rows][cols]) {
+  static int i;
+  static int j;
   printf(" __________________________________ \n");
-  for (int i = 1; i <= 6; i++) {
-    //if (i == row) {
-
-    //}
-    printf("|    |    |    |    |    |    |    |\n");
-    printf("|    |    |    |    |    |    |    |\n");
-    printf("|____|____|____|____|____|____|____|\n");
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      printf("%c", board[i][j]);
+    }
+    printf("\n");
   }
+}
+
+int placePiece(int rows, int cols, char board[rows][cols], int colPlayed) {
+  static int k;
+  if (board[rows - 1][colPlayed] == 'z') {
+    return (rows - 1);
+  }
+  else {
+    for (k = 0; k < rows; k++) {
+      if ((board[k][colPlayed] == 'z') && (board[k + 1][colPlayed] != 'z')) {
+        return k;
+      }
+    }
+  }
+  return 0; //will never be reached
+}
+
+bool checkWinningPosition(int rows, int cols, char board[rows][cols], int start_row, int start_col) {
+  static char piece;
+  piece = board[start_row][start_col];
+  static int count;
+  count = 0;
+  int a;
+
+  if (start_row >= 4) {
+    for (a = 1; a < 4; a++) {
+      if (piece == board[start_row - a][start_col]) count++;
+      if (count == 3) return 1;
+    }
+  }
+  count = 0;
+  if ((rows - start_row) >= 4) {
+    for (a = 1; a < 4; a++) {
+      if (piece == board[start_row + a][start_col]) count++;
+      if (count == 3) return 1;
+    }
+  }
+  count = 0;
+  if (start_col >= 3) {
+    for (a = 1; a < 4; a++) {
+      if (piece == board[start_row][start_col - a]) count++;
+      if (count == 3) return 1;
+    }
+  }
+  count = 0;
+  if ((cols - start_col) >= 4) {
+    for (a = 1; a < 4; a++) {
+      if (piece == board[start_row][start_col + a]) count++;
+      if (count == 3) return 1;
+    }
+  }
+  return 0;
 }
 
 int main(void) {
@@ -57,7 +95,14 @@ int main(void) {
 
   int menu_select;
   int play_col;
-  //struct node* Piece = (struct node*)malloc(sizeof(struct node));
+  int play_row;
+  bool player;
+
+  int rows = 7;
+  int cols = 6;
+
+  char board[rows][cols];
+  memset(board, 'z', sizeof board);
 
   select_again:
   scanf("%d", &menu_select);
@@ -66,36 +111,44 @@ int main(void) {
       printf("1");
       break;
     case 2:
-      createBoardScreen();
-      //enter number of rows
-      //enter number of columns
-      //error checking for r,c < 0 or r,c too large
-      printf("What column would you like to play1 in?");
-      scanf("%d", &play_col);
-      /*if (play_col == 1) {
-        *Piece->xCoor = 1;
-        *Piece->topLeft = NULL;
-        *Piece->left = NULL;
-        *Piece->botLeft = NULL;
-        *Piece->player1 = 1;
-        *Piece->player2 = 0;
-        *Piece->filled = 1;
-      *}
-      *else if (play_col == 7) {
-      *  Piece->xCoor = 7;
-      *  Piece->topRight = NULL;
-      *  Piece->right = NULL;
-      *  Piece->botRight = NULL;
-      *  Piece->player1 = 1;
-      *  Piece->player2 = 0;
-      *  Piece->filled = 1;
-      *}
-      *else {
-      *  Piece->xCoor = play_col;
-      *  Piece->player1 = 1;
-      *  Piece->player2 = 0;
-      *  Piece->filled = 1;
-    }*/
+      player = 0;
+      createBoardScreen(rows, cols, board);
+        while (1) {
+          if (!player) {
+            printf("Player 1's turn.\n");
+          }
+          else if (player) {
+            printf("Player 2's turn.\n");
+          }
+          reselect:
+          printf("What column would you like to play in?");
+          scanf("%d", &play_col);
+          if ((play_col < 0) || (play_col > cols)) {
+            printf("That is not a valid column to play in.\n");
+            goto reselect;
+          }
+          else if (board[0][play_col - 1] != 'z') {
+            printf("That column is full.\n");
+            goto reselect;
+          }
+          play_col--;
+          play_row = placePiece(rows, cols, board, play_col);
+          if (!player) {
+            board[play_row][play_col] = 'x';
+          }
+          else if (player) {
+            board[play_row][play_col] = 'o';
+          }
+          createBoardScreen(rows, cols, board);
+          if (checkWinningPosition(rows, cols, board, play_row, play_col)) break;
+          player = !player;
+        }
+        if (!player) {
+          printf("Player 1 wins!");
+        }
+        else if (player) {
+          printf("Player 2 wins!");
+        }
       break;
     case 3:
       printf("3");
@@ -108,9 +161,5 @@ int main(void) {
       createMenuScreen();
       goto select_again;
   }
-
-
-
-
   return 0;
 }
